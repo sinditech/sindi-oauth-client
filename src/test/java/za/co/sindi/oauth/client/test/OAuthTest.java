950faa1dc43f8842795cd3ca9f1f8df8cdf8cbf6
Buhake Sindi <buhake.sindi@sindi.co.za>
2024-03-27 01:11:51 +0200

    Initial commit.

--- src/test/java/za/co/sindi/oauth/client/test/OAuthTest.java
+++ src/test/java/za/co/sindi/oauth/client/test/OAuthTest.java
@@ -29,15 +29,15 @@ public class OAuthTest {
 //		System.out.println(uri);
 //		
 //		HttpClient client = new HttpClientImpl();
-//		HttpRequest request = new HttpRequestImpl(HttpMethod.GET, URI.create("https://www.facebook.com/v19.0/dialog/oauth?response_type=code&client_id=381571044440&state=xyz&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb").toURL());
+//		HttpRequest request = new HttpRequestImpl(HttpMethod.GET, URI.create("https://www.facebook.com/v19.0/dialog/oauth?response_type=code&client_id=fdfgdfsgdfgdfg&state=xyz&redirect_uri=https%3A%2F%2Fclient%2Eexample%2Ecom%2Fcb").toURL());
 //		request.setHeader(HttpHeaderName.ACCEPT, "application/json");
 //		HttpResponse response = client.send(request);
 //		System.out.println(response.getStatusCode());
 		
-		Builder builder = new Builder("https://oauth2.googleapis.com/token", "4/0AfJohXnEuhC_I7QuJ5jvhhPKOqgOjsjOgK5huITa0-BXe2CrxfBx3kuuRagxwu90IR9Kpw", "https://developers.google.com/oauthplayground/");
+		Builder builder = new Builder("https://oauth2.googleapis.com/token", "dfasdfasdfsadfsdf", "https://developers.google.com/oauthplayground/");
 		AuthorizationCodeAccessTokenRequest accessTokenRequest = builder.build();
 		AuthorizationCodeAccessTokenRequestClient oauthClient = new AuthorizationCodeAccessTokenRequestClient();
-		oauthClient.setClientAuthentication(new ClientParametersAuthentication("407408718192.apps.googleusercontent.com", null));
+		oauthClient.setClientAuthentication(new ClientParametersAuthentication("sadfasfasdfadsfdsf.apps.googleusercontent.com", null));
 		AccessTokenResponse accessTokenResponse = oauthClient.send(accessTokenRequest);
 	}
 }
